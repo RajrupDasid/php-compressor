2cf0f4dd0208bf182d8a46af8b8cfc603accf5f2
RajrupDasid <rajrupofficialid@protonmail.com>
2023-08-18 16:37:27 +0530

    Php conditional if statement
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

---
+++ ifstatement.php
@@ -0,0 +1,20 @@
+<?php 
+// $age = -20;
+// if ($age >= 20){
+//     echo "You may enter this site";
+
+// }
+// elseif($age == 0){
+//     echo "you have just born";
+// }else{
+//     echo "Huh?! Fuck you";
+// }
+
+$adult = true;
+if($adult){
+    echo "You may enter this site";
+}else{
+    echo "You must be an adult to enter this site";
+}
+?>
+
