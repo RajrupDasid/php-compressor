e8f3f25e2365f1bf716b138d1d70ddb2a57d0eb0
RajrupDasid <rajrupofficialid@protonmail.com>
2023-08-18 16:40:52 +0530

    PHP switch case
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

---
+++ switch.php
@@ -0,0 +1,39 @@
+<?php
+// $grade = "A";
+// switch ($grade){
+//     case "A";
+//         echo  "You did great";
+//         break;
+//     case "B";
+//         echo "You did good";
+//         break;
+//     case "C";
+//         echo "It's ok";
+//         break;
+//     case "D";
+//         echo "you failed";
+//         break;
+//     case "F";
+//         echo "You failed badly";
+//         break;
+//     default:
+//         echo "{$grade} is not valid";    
+//}
+
+    $date =date("1");
+    $date = "Saturday";
+    switch($date){
+        case "Monday":
+            echo "I hate mondays";
+            break;
+        case "Tueday":
+            echo "It's  Taco Tuesday";
+            break;
+        case "Saturday":
+            echo "It's fucking party time bro";
+            break;
+        case "Sunday":
+                echo "Lotta drunken bro";
+                break;
+    }
+?>
