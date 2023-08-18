1bb07fc392ef5da4cd0594da6f5614bd00be839d
RajrupDasid <rajrupofficialid@protonmail.com>
2023-08-18 16:35:59 +0530

    Php oops concepts
    Signed-off-by: RajrupDasid <rajrupofficialid@protonmail.com>

---
+++ functions.php
@@ -0,0 +1,22 @@
+<?php
+//function = write some code once, reuse when you need it 
+//type() after function name to invoke
+//ex. add() subtract() multiply divide()
+
+// function motogp(){
+//         echo "Motogp is great";
+//     }
+//     motogp();
+
+// function identity($name, $age){
+//     echo "Hello {$name}";
+// }
+
+// identity("Rajrup", 25);
+
+// function even_odd($num){
+//     $result = $num % 2;
+//     return $result;
+// }
+// echo even_odd(11);
+?>
